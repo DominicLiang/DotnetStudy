683dbce0841b84c95dc9caf91664e83c9311f672
lhr <76876862+lhr667@users.noreply.github.com>
2023-05-26 16:24:28 +0800

    Delete Program.cs

---
+++ 10-异步和多线程/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _10_异步和多线程
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
