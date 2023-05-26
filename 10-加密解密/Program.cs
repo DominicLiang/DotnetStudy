1e1933aa9c568eb74fc763d8375f7b399afa0ddd
lhr <76876862+lhr667@users.noreply.github.com>
2023-05-26 15:50:13 +0800

    更新

--- 10-加密解密/Program.cs
+++ 10-加密解密/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace _10_加密解密
+﻿using System.Security.Cryptography;
+
+namespace _10_加密解密;
+
+//1.MD5     不可逆加密
+//2.Des     对称可逆加密
+//3.RSA     非对称可逆加密
+//4.数字证书  SSL
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // MD5
+            Console.WriteLine();
         }
     }
 }
