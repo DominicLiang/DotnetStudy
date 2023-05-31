2505910cdc9972a3a30b4a123866d2c736811608
HyruleL <76876862+HyruleL@users.noreply.github.com>
2023-05-31 17:20:24 +0800

    Update Study.sln

--- 14-SignalR/Program.cs
+++ 14-SignalR/Program.cs
@@ -17,7 +17,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 {
-    builder.Services.AddSignalR();
+    //解决分布式问题,需要Redis
+    //包：Microsoft.AspNetCore.SignalR.StackExchangeRedis
+
+    builder.Services.AddSignalR().AddStackExchangeRedis("localhost", opt =>
+    {
+        opt.Configuration.ChannelPrefix = "SignalR_";
+    });
 
     string[] urls = new[] { "http://localhost:5173" };
     builder.Services.AddCors(options =>
--- SignalRFrontEnd/src/views/homeView.vue
+++ SignalRFrontEnd/src/views/homeView.vue
@@ -1,7 +1,43 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from 'vue'
+import * as signalR from '@microsoft/signalr'
+
+let connection
+const userMsg = ref()
+const msg = ref([])
+
+const onKeyPress = async (e) => {
+  if (e.keyCode != 13) return
+  await connection.invoke('SendPublicMsg', userMsg.value)
+  //   对应服务器的SendPublicMsg方法
+  userMsg.value = ''
+}
+
+onMounted(async () => {
+  connection = new signalR.HubConnectionBuilder()
+    .withUrl('https://localhost:7285/MyHub', {
+      skipNegotiation: true,
+      transport: signalR.HttpTransportType.WebSockets
+    })
+    .withAutomaticReconnect()
+    .build()
+  await connection.start()
+  connection.on('PublicMsgReceived', (m) => {
+    msg.value.push(m)
+  })
+})
+</script>
 
 <template>
-    homeview11111
+  <!-- npm install @microsoft/signalr 需要安装singnalr包-->
+  <input type="text" v-model="userMsg" @keypress="onKeyPress" />
+  <div>
+    <ul>
+      <li v-for="(m, index) in msg" :key="index">
+        {{ m }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped></style>
