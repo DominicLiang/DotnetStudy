6c253a41f68a5f1d8fa70c517347ea32a337cee9
HyruleL <76876862+HyruleL@users.noreply.github.com>
2023-05-31 17:17:02 +0800

    Delete ASP.NET MVC入门 directory

--- ASP.NET MVC入门/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASP.NET_MVC入门.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
